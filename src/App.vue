5b154ae2ff6e3f3174abc597baa9fc8ce49c1b13
Alvaro Rodriguez <arod.gzz28@gmail.com>
2020-10-28 23:08:18 -0600

    I went back to having only the frontend in vue.js, to be able to deploy in github

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <navigation/>
+    <Lander />
+    <router-view></router-view>
+    <Footer/>
+    
+  </div>
+</template>
+
+<script>
+import Navigation from './components/includes/nav.vue'
+import Lander from './components/Lander.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navigation,
+    Lander
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Lander.vue
@@ -0,0 +1,248 @@
+<template>
+    <div>
+        <!-- lander section -->
+        <section class="section">
+            <div class="contact">
+                <img alt="Vue logo" class="rounded-circle" src="./images/photo.png">
+                <a href="https://www.linkedin.com/in/alvarodgzz/">
+                <img alt="Vue logo" class="rounded-circle" src="./images/linkedin.png" width="70" height="70" style="vertical-align:middle;margin:50px 0px">
+                </a>
+                <a href="https://github.com/alvarodgzz">
+                <img alt="Vue logo" class="rounded-circle" src="./images/github.png" width="70" height="70" style="vertical-align:middle;margin:50px 0px">
+                </a>
+                <a href="https://www.instagram.com/alvarock_/">
+                <img alt="Vue logo" class="rounded-circle" src="./images/insta.png" width="70" height="70" style="vertical-align:middle;margin:50px 0px">
+                </a>
+                <h1>CS at Tec de Monterrey<br>Software Architecture Intern at Chubb</h1>
+                <a href="mailto:arod.gzz28@gmail.com">arod.gzz28@gmail.com<br></a>
+                <a href="https://drive.google.com/file/d/1mO-B0gNTi81PSV4FY-EK2548xqsITmTR/view?usp=sharing" download="resumeAlvaroRdz2020">Resume</a>
+                <p>+52 81 20 63 71 39</p>
+                <!--<a href="mailto:raunak.00o201@gmail.com"> Email_id </a>-->
+                <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target=".bd-example-modal-lg">Contact</button>
+                
+            </div>
+            
+        </section>
+
+        
+
+        <!-- portfolio section -->
+        <section class="protfolio">
+            <h1>Work and Industry Experiences</h1>
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-head">
+                                    <img alt="Vue logo" class="rounded-circle img-custome" src="./images/chubbLogo.png">
+                                    <h2>Software Architecture Intern at CHUBB</h2>
+                                </div>
+                                <hr>
+                                <div class="card-body">
+                                    <p>
+                                        Responsible for designing software solution diagrams and running conceptual tests on a Citibanamex/Costco alliance
+                                        project expected to increase its sales projection by 22 percent.
+                                        Supported the administration of the applications’ inventory and contributed to the documentation of software architecture solutions.
+                                    </p>
+                                </div>
+                            </div>
+                            <br>
+                    </div>
+
+                    <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-head">
+                                    <img alt="Vue logo" class="rounded-circle img-custome" src="./images/google.png">
+                                    <h2>Google's Software Product Sprint (SPS) Participant</h2>
+                                </div>
+                                <hr>
+                                <div class="card-body">
+                                    <p>
+                                        Collaborated with a Google Software Engineer to create a personal portfolio using HTML, CSS, JavaScript, and
+                                        leveraging Google Cloud Platform APIs such as AppEngine and DataStore.
+                                        Experienced industry-leading practices such as: contributing to open-source software using Git and GitHub, conducting
+                                        regular code reviews with my mentor, and designing components and interfaces.
+                                    </p>
+                                </div>
+                            </div>
+                        <br>
+                    </div>  
+
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-head">
+                                <img alt="Vue logo" class="rounded-circle img-custome" src="./images/twitter.png">
+                                <h2>Twitter's TechProud Participant</h2>
+                            </div>
+                            <hr>
+                            <div class="card-body">
+                                <p>
+                                    One of 25 attendees selected from over 1000+ applicants to participate in a two-day program for CS students aspiring a career in tech. The program provided resources to develop technical and professional skills as well as networking with all the fellow participants.
+                                    Participated in the ReadySetCode, a competitive programming team challenge, designed by Twitter Engineers where we used Python3 and Java8.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <h1>Relevant Projects</h1>
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-head">
+                                    <img alt="Vue logo" class="rounded-circle img-custome" src="./images/covid.png">
+                                    <h2>Covid Status IOS App</h2>
+                                </div>
+                                <hr>
+                                <div class="card-body">
+                                    <p>
+                                        Developed an IOS App for users to know their level of risk against Covid19 based on their health and personal data
+                                        by comparing with a prediction machine learning model trained with Mexico’s health system database.
+                                        Worked on the IOS App using Swift 5.0 with the UIKit and Cocoa components. Responsible for integrating the Python
+                                        ML model into the IOS environment using PythonKit libraries.
+                                    </p>
+                                    <button type="button" class="btn btn-outline-secondary" onclick="window.location.href='https://github.com/alvarodgzz/DAKOhackmty2020covidStatus';">
+                                        Source Code
+                                    </button>
+                                   <!-- <button type="button" class="btn btn-outline-secondary">More</button> -->
+                                </div>
+                            </div>
+                            <br>
+                    </div>
+
+                    <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-head">
+                                    <img alt="Vue logo" class="rounded-circle img-custome" src="./images/brick25.png">
+                                    <h2>Balanced Physics IOS App</h2>
+                                </div>
+                                <hr>
+                                <div class="card-body">
+                                    <p>
+                                        Created an interactive iPhone app for an ITESM professor and for his over 150 students, so they can learn about Balance physics concepts on their phones in a more interactive way.
+                                        Worked with Swift and Objective-C for the app development, as well as using UIKit components such as UIViews, UIImageViews, and Gesture Recognizers with the Cocoa Framework
+                                    </p>
+                                    <button type="button" class="btn btn-outline-secondary" onclick="window.location.href='https://github.com/alvarodgzz/ProyectoFinalEquilibrio';">
+                                        Source Code
+                                    </button>
+                                </div>
+                            </div>
+                        <br>
+                    </div>  
+
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-head">
+                                <img alt="Vue logo" class="rounded-circle img-custome" src="./images/twitter.png">
+                                <h2>BattleOfTheCamps Hackathon at Twiiter</h2>
+                            </div>
+                            <hr>
+                            <div class="card-body">
+                                <p>
+                                    Won the HackForGood category on Twitter’s first virtual Hackathon with our ”Stay Informed Just to Feel Something” feature, that filtered truthful tweets by sentiment analysis. Responsible for the Twitter API integration and the front end design using Bootstrap with Flask.
+                                    Worked with IBM’s Tone Analyzer API and a CNN model to determine if a tweet was trustworthy. Used Tweepy for the API along with Python3 for overall development.
+                                </p>
+                                <!--<button type="button" class="btn btn-outline-secondary">More</button>-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- modal for contact -->
+        
+        <section class="">
+            <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content">
+                        <talk/>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section">
+            <div class="skills">
+                <div class="row">
+
+                        <div class="card">
+                            <div class="card-head">
+                                <img alt="Vue logo" class="rounded-circle img-custome" src="./images/skills.png">
+                                <h2>Skills</h2>
+                            </div>
+                            <hr>
+                            <div class="card-body">
+                                <h1>Programming languages and Technologies</h1>
+                                <p>
+                                    C++ (3 years), Java (1.5 years), JavaScript (6 months), HTML/CSS (1 year), SQL (6 months), Python (6 months), Swift (6 months), Objective-C (6 months), Scilab (6 months), MongoDB, Git, Flask, MySql, Tableau, Oracle Sql.
+                                </p>
+                                <h1>Languages</h1>
+                                <p>
+                                    English (Advanced), German (Beginner), Spanish (Native).
+                                </p>
+                            </div>
+                        </div>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        
+    </div>
+
+    
+</template>
+
+<script lang="ts">
+import talk from './Contact.vue'
+//import postService from '../postService'
+export default {
+    name:'Lander',
+    /*data() {
+            return {
+                posts: [],
+                error: '',
+                name: '',
+                email: ''
+            }
+        },
+        async created() {
+            try {
+                this.posts = await postService.getPosts();
+            } catch(err) {
+                this.error = err;
+            }
+        },*/
+    components:{
+        talk,
+    }  
+}
+</script>
+
+<style lang="css" scoped>
+    .section{
+        padding-top: 10%;
+        padding-bottom: 10%;
+    }
+    
+
+    .protfolio{
+        background-color: #1b21d74f;
+        padding-top: 8%;
+        padding-bottom: 15%;
+        /* padding:15% */
+    }
+
+    .img-custome{
+        width: 140px;
+        height: 140px;
+        margin-top: 15px;
+    }
+
+</style>
